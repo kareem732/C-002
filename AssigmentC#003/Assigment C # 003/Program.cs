3b113ef8cd9a86242a1b5fd2c6159b58a934e461
Apple <140983808+kareem732@users.noreply.github.com>
2025-06-30 18:45:25 +0300

    Add Assignment C# 003

---
+++ AssigmentC#003/Assigment C # 003/Program.cs
@@ -0,0 +1,126 @@
+﻿namespace Assigment_C___003
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //////////////////////////////////////////////////////////////
+
+            /* Console.Write("Dude Please Enter your number but it must be divided by 3 and 4 : ");
+             bool isValidNumber = int.TryParse(Console.ReadLine(), out int number);
+             if (isValidNumber)
+             {
+                 if (number % 3 == 0 && number % 4 == 0)
+                 {
+                     Console.WriteLine("Yes");
+                 }
+                 else
+                 {
+                     Console.WriteLine("No");
+                 }
+             }
+             else
+             {
+                 Console.WriteLine("Invalid input, please enter a valid integer.");
+             }
+            */
+
+            //////////////////////////////////////////////////////////////
+
+            /*Console.Write("dude please enter your number : ");
+            bool isValidNumber = int.TryParse(Console.ReadLine(), out int number);
+            if (number > 0)
+            {
+                Console.WriteLine("positive");
+            }
+            else
+            {
+                Console.WriteLine("negative");
+            }
+            */
+            //////////////////////////////////////////////////////////////
+            /*
+             Console.Write("Enter your 1st number : ");
+             bool isValidFirstNumber = int.TryParse(Console.ReadLine(), out int firstNumber);
+             Console.Write("Enter your 2nd number : ");
+             bool isValidSecondNumber = int.TryParse(Console.ReadLine(), out int secondNumber);
+             Console.Write("Enter your 3rd number : ");
+             bool isValidThirdNumber = int.TryParse(Console.ReadLine(), out int thirdNumber);
+             if (firstNumber > secondNumber && firstNumber > thirdNumber)
+             {
+                 Console.WriteLine($"Max element = {firstNumber}");
+                 if (secondNumber<thirdNumber)
+                 {   
+                 Console.WriteLine($"MIN element = {secondNumber}");
+                 }else
+                 {
+                     Console.WriteLine($"MIN element = {thirdNumber}");
+                 }
+             }
+
+             else if (secondNumber > firstNumber && secondNumber > thirdNumber)
+             {
+                 Console.WriteLine($"Max element = {secondNumber}");
+                 if (firstNumber < thirdNumber )
+                 {
+                 Console.WriteLine($"MIN element = {firstNumber}");
+                 }
+                 else
+                 {
+                     Console.WriteLine($"MIN element = {thirdNumber}");
+                 }
+             }
+             else if (thirdNumber > firstNumber && thirdNumber > secondNumber)
+             {
+                 Console.WriteLine($"Max element = {thirdNumber}");
+                 if (firstNumber < secondNumber)
+                 {
+                     Console.WriteLine($"MIN element = {firstNumber}");
+                 }
+                 else
+                 {
+                     Console.WriteLine($"MIN element = {secondNumber}");
+                 }
+             }
+             else
+             {
+                 Console.WriteLine("All numbers are equal.");
+             }
+            */
+            //END
+
+            /*Console.Write("Enter your number : ");
+            bool number = int.TryParse(Console.ReadLine(),out int num);
+            if (number)
+            {
+                if (num % 2 == 0 )
+                {
+                    Console.WriteLine("even");
+                }else
+                {
+                    Console.WriteLine("odd");
+                }
+            }
+            */
+
+            //END
+
+            //Console.Write("Enter your Charachter : ");
+
+            //string charachter = Console.ReadLine()!;
+
+            //if (charachter == "o" || charachter == "O" || charachter == "A" || charachter == "a" || charachter == "e" || charachter == "E" || charachter == "u" || charachter == "U" || charachter == "I" || charachter == "i") 
+            //{
+            //    Console.WriteLine("vowel");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Consonant");
+            //}
+
+            //END
+
+
+        }
+    }
+}
